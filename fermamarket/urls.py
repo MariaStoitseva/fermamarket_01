957047b795c017e0dde3a37675fe7af3d962f3b3
Maria <mimozka04@gmail.com>
2025-05-20 15:35:49 +0300

    add

--- fermamarket/urls.py
+++ fermamarket/urls.py
@@ -8,6 +8,10 @@ from fermamarket import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('terms/', TemplateView.as_view(template_name='info/terms.html'), name='terms'),
+    path('privacy/', TemplateView.as_view(template_name='info/privacy.html'), name='privacy'),
+    path('shipping/', TemplateView.as_view(template_name='info/shipping.html'), name='shipping'),
+    path('farmer-requirements/', TemplateView.as_view(template_name='info/farmer_requirements.html'), name='farmer_requirements'),
     path('accounts/', include('fermamarket.customusers.urls')),
     path('farmer/', include('fermamarket.farmers.urls')),
     path('client/', include('fermamarket.clients.urls')),
